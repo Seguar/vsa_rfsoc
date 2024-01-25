d65631fd3e397ca09c2eda354f062dd9703675c5
Seguar <69475749+Seguar@users.noreply.github.com>
2024-01-25 15:32:24 +0200

    Condition added (need to rewrite)

--- sigFinder.m
+++ sigFinder.m
@@ -1,25 +1,26 @@
 function [sig, fb_lines, fe_lines, f_len, f_wid] = sigFinder(sigRec, findCoef, minLen)
-    findTrh = mean(abs(sigRec))*findCoef;
-    cutInd = abs(sigRec) >= findTrh;
-    buff(cutInd) = 1;
-    gap = 1;
-    pack = 1;
-    sig = [];
-    for i = 1:length(buff)
-        if (buff(i) == 0)
-          gap = gap + 1;  
+findTrh = mean(abs(sigRec))*findCoef;
+cutInd = abs(sigRec) >= findTrh;
+buff(cutInd) = 1;
+gap = 1;
+pack = 1;
+sig = [];
+for i = 1:length(buff)
+    if (buff(i) == 0)
+        gap = gap + 1;
+    else
+        if gap > minLen
+            sig(pack,:) = [i,gap];
+            gap = 0;
+            pack = pack + 1;
         else
-            if gap > minLen
-                sig(pack,:) = [i,gap];
-                gap = 0;
-                pack = pack + 1;
-            else
-                gap = 0;
-            end
+            gap = 0;
         end
     end
+end
 
-    sig(pack,:) = [i,gap]; % Last part
+sig(pack,:) = [i,gap]; % Last part
+if size(sig)>1
     f_wid = median(sig(:,2));
     fb_lines = sig(:,1);
     fe_lines = sig(2:end,1) - sig(2:end,2); % Works on any lengths
@@ -31,9 +32,15 @@ function [sig, fb_lines, fe_lines, f_len, f_wid] = sigFinder(sigRec, findCoef, m
         fe_lines(end) = fb_lines(end);
         fb_lines = fb_lines(1:end-1);
     end
-if not(isempty(fe_lines))
-    if fe_lines(end)-fb_lines(end) < f_len
-        fb_lines = fb_lines(1:end-1);
-        fe_lines = fe_lines(1:end-1);
+    if not(isempty(fe_lines))
+        if fe_lines(end)-fb_lines(end) < f_len
+            fb_lines = fb_lines(1:end-1);
+            fe_lines = fe_lines(1:end-1);
+        end
     end
+else
+    fb_lines = 1;
+    fe_lines = length(sigRec);
+    f_len = length(sigRec);
+    f_wid = length(sigRec);
 end
