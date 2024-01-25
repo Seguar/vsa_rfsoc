88e088c4a74c4e2f10cec3c556fc3011719199e8
Seguar <69475749+Seguar@users.noreply.github.com>
2024-01-25 16:35:31 +0200

    Deleted not used lines, added bf switch

--- rfsocBf.m
+++ rfsocBf.m
@@ -1,64 +1,31 @@
 function [yspec, estimated_angle, bfSig] = rfsocBf(app, vsa, ch, bf, off, gap, cutter, ang_num, estimator, data_v, tcp_client)
 test_z = zeros(1, gap);
-
-setupFile = 'ofdm_iq_20_cal.setx'; %1ch_ddc_len.setx iqtools_ofdm_qam16_200
-bw = 20e6;
 fc = 5.7e9;
-fsRfsoc = 125e6;
 dataChan = 2^14; % Samples per channel (buffer size)
 
 c = physconst('LightSpeed'); % propagation velocity [m/s]
 lambda = c / fc; % wavelength
-d = lambda/2; % spacsing antenna elemnts
-min_ang = -90; % min scanning angle
-max_ang = 90; % max scanning angle
-scan_res = 1; % scan resolution
-scan_axis = min_ang:scan_res:max_ang; % angles axis
-num_elements = 4;
-ula = phased.ULA('NumElements',num_elements,'ElementSpacing',d, 'ArrayAxis','y');
 
-%% AFunctions
-cPhSh = @(a) 360*(lambda/2)*sind(a)/lambda; % Calculation of constant phase shift between elements
-deg2comp = @(a) exp(1i*deg2rad(a)); % Degrees to complex (1 round) convertion
 
 %% TCP prep
 data_size = dataChan * 8;
-data_size_bytes = typecast(uint64(data_size), 'uint8');
-
 channels = 8;
-raw = zeros(channels, data_size / 8);
-% Define the server IP and port (should match the server settings)
-server_ip = 'pynq'; % Use the appropriate IP address or hostname
-server_port = 4000; % Use the same port number used in the Python server
 dataLen = data_size/channels;
 %% TCP
 rawData = tcpDataRec(tcp_client, data_size, channels);
-%% Matlab MVDR FUNC
+%% Matlab MVDR DOA FUNC
 [yspec, estimated_angle] = estimator(rawData'*rawData);
 %% Angles
-df = cPhSh(-estimated_angle(ang_num));
-an(1) = 1;
-an(2) = deg2comp(df*1);
-an(3) = deg2comp(df*2);
-an(4) = deg2comp(df*3);
-rawDataAdj(:,1) = rawData(:,1)*an(1);
-rawDataAdj(:,2) = rawData(:,2)*an(2);
-rawDataAdj(:,3) = rawData(:,3)*an(3);
-rawDataAdj(:,4) = rawData(:,4)*an(4);
 
 if ch>4
-    if bf
-        rawSum = sum(rawDataAdj, 2);
-    else
-        rawSum = sum(rawData, 2);
-    end
-else
-    if bf
-        rawSum = rawDataAdj(:,ch);
-    else
-        rawSum = rawData(:,ch);
-    end
+    ch = 1:4;
 end
+
+if bf
+    rawData = steerBf(rawData, estimated_angle(ang_num), lambda);
+end
+rawSum = sum(rawData(:,ch), 2);
+
 
 %% Cutter
 if (cutter)
