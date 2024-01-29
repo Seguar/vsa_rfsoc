96e0976e70c1949a678e1b3a70f26926899daf0d
Seguar <69475749+Seguar@users.noreply.github.com>
2024-01-29 14:18:37 +0200

    Unused code reduction

--- Packet-Creator-VHT/Signal_Gen.m
+++ Packet-Creator-VHT/Signal_Gen.m
@@ -1,16 +1,6 @@
 function [packet] = Signal_Gen(Fs)
-    
-    % addpath([pwd '\Packet-Creator-VHT']);
 
     outputPower     = 10; %[dBm]
-    % Fs              = 64e9/24/44;%[Hz]
-%     Fs              = 640e6;%[Hz]
-%     Fs              = 61.44e6;%[Hz]
-%     Fs              = 60e6;%[Hz]
-%     Fs              = 30e6;%[Hz]
-%     Fs              = 62.5e6;%[Hz]
-%     Fs              = 150e6;%[Hz]
-%     Fs              = 4e9;%[Hz]
 
     frameLength     = 500; %1e3; % 5e3;
     BW              = 20;%[MHz]
@@ -25,14 +15,6 @@ function [packet] = Signal_Gen(Fs)
     refSignal_norm  = rawSignal./rms(rawSignal);
     refSignal       = refSignal_norm.*dBm2V(outputPower);
 
-    % figure
-    %     hold on
-    %   spectrumAnalyzer(refSignal, Fs, 'r');
-    %     xlim([-5000 5000]);
-    %     ylim([-90 0]);
-%         legend('Ideal Signal');
-%         grid minor
-%         hold off
     packet = wavaform;
     % packet = csvread([num2str(BW),'MHz_',num2str(Fs*1e-6),fname,'.csv']);
     %save([num2str(BW),'MHz',num2str(Fs*1e-6),fname,'.mat'],'packet');
