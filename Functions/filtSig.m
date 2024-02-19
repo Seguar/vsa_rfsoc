7d6ea71e5dc7f6536d5e59685e24cf4fc70af5b4
Seguar <69475749+Seguar@users.noreply.github.com>
2024-02-19 19:47:25 +0200

    Bandwidth bug fixed

--- Functions/filtSig.m
+++ Functions/filtSig.m
@@ -1,7 +1,7 @@
 function sigFilt = filtSig(sig, fs, bw)
     fStep = fs/length(sig);
     sigFd = fftshift(fft(sig));
-    sigNewLim = round((fs/2-bw)/fStep:(fs/2+bw)/fStep);
+    sigNewLim = round((fs/2-bw/2)/fStep:(fs/2+bw/2)/fStep);
     sigNew = zeros(size(sig));
     sigNew(sigNewLim,:) = sigFd(sigNewLim,:);
     sigFilt = ifft(ifftshift(sigNew));
