01f1e233866dd659c5cbe410aa7c305f28934308
Seguar <69475749+Seguar@users.noreply.github.com>
2024-02-23 12:11:33 +0200

    Wrong sign fixed

--- Packet-Creator-VHT/Signal_Gen.m
+++ Packet-Creator-VHT/Signal_Gen.m
@@ -11,7 +11,7 @@ function [packet] = Signal_Gen(Fs)
 
     %wavaform        = VHT_Creator(MCS, BW, frameLength, Fs/1e6, isDataRand,[pwd '\',num2str(BW),'MHz_',num2str(Fs*1e-6),fname,'.csv']);
     wavaform        = VHT_Creator(MCS, BW, frameLength, Fs/1e6, isDataRand,['']);
-    rawSignal       = [zeros(1,1); wavaform(:,1) + 1i.*wavaform(:,2); zeros(floor(length(wavaform(:,1))*0.1),1)];
+    rawSignal       = [zeros(1,1); wavaform(:,1) - 1i.*wavaform(:,2); zeros(floor(length(wavaform(:,1))*0.1),1)];
     refSignal_norm  = rawSignal./rms(rawSignal);
     refSignal       = refSignal_norm.*dBm2V(outputPower);
 
