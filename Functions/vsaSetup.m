bd0d2865b19c5459ca4f9c3a50247d665ac64f4b
Seguar <69475749+Seguar@users.noreply.github.com>
2024-02-25 13:02:10 +0200

    Setup name in ttitle added

--- Functions/vsaSetup.m
+++ Functions/vsaSetup.m
@@ -6,3 +6,4 @@ format long g
 pcvsa = visa('keysight', 'TCPIP0::localhost::hislip_vsa0::INSTR');
 fopen(pcvsa)
 fprintf(pcvsa, sprintf(':MMEMory:LOAD:SETup "%s"', setupFile));
+fprintf(pcvsa, sprintf(':DISPlay:ANNotation:TITLe:DATA "%s"', setupFile));
