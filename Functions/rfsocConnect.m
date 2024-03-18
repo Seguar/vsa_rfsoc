ccdcfa179e2025e9457c87206a5707f2b010b154
Seguar <69475749+Seguar@users.noreply.github.com>
2024-03-18 17:58:10 +0200

    USB-connection added

--- Functions/rfsocConnect.m
+++ Functions/rfsocConnect.m
@@ -1,7 +1,11 @@
 function tcp_client = rfsocConnect(server_ip, server_port, dataChan)
 %% TCP prep
 % Define the server IP and port (should match the server settings)
-tcp_client = tcpclient(server_ip, server_port);
+try
+    tcp_client = tcpclient(server_ip, server_port, "ConnectTimeout", 5);
+catch
+    tcp_client = tcpclient('192.168.3.1', server_port, "ConnectTimeout", 5); % USB connection
+end
 curr_data_size = dataChan * 8;
 curr_data_size_bytes = typecast(uint64(curr_data_size), 'uint8');
 write(tcp_client, curr_data_size_bytes);
