7d735fb6f113c68302a86688d7d965c84731a6fb
Seguar <69475749+Seguar@users.noreply.github.com>
2024-03-19 14:54:14 +0200

    Connection timeout is increased

--- Functions/rfsocConnect.m
+++ Functions/rfsocConnect.m
@@ -2,9 +2,9 @@ function tcp_client = rfsocConnect(server_ip, server_port, dataChan)
 %% TCP prep
 % Define the server IP and port (should match the server settings)
 try
-    tcp_client = tcpclient(server_ip, server_port, "ConnectTimeout", 5);
+    tcp_client = tcpclient(server_ip, server_port, "ConnectTimeout", 10);
 catch
-    tcp_client = tcpclient('192.168.3.1', server_port, "ConnectTimeout", 5); % USB connection
+    tcp_client = tcpclient('192.168.3.1', server_port, "ConnectTimeout", 10); % USB connection
 end
 curr_data_size = dataChan * 8;
 curr_data_size_bytes = typecast(uint64(curr_data_size), 'uint8');
