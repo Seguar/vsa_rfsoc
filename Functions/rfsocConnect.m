c26804ca5a5bf494de4f468f0a17e5f9ca7476a4
Seguar <1035707@gmail.com>
2024-03-28 12:07:06 +0200

    ConnectTimeout is reduced

--- Functions/rfsocConnect.m
+++ Functions/rfsocConnect.m
@@ -2,8 +2,8 @@ function tcp_client = rfsocConnect(server_ip, server_port, commands)
 %% TCP prep
 % Define the server IP and port (should match the server settings)
 try
-    tcp_client = tcpclient(server_ip, server_port, "ConnectTimeout", 10);
+    tcp_client = tcpclient(server_ip, server_port, "ConnectTimeout", 1);
 catch
-    tcp_client = tcpclient('192.168.3.1', server_port, "ConnectTimeout", 10); % USB connection
+    tcp_client = tcpclient('192.168.3.1', server_port, "ConnectTimeout", 1); % USB connection
 end
 writeline(tcp_client, commands);
