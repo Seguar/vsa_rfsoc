4219440ce15cc25107d23f8c4e010915e4b930cc
Seguar <1035707@gmail.com>
2024-08-27 12:06:14 +0300

    IQtools removed

--- iqtools_2023_10_24/iqtools/iqgeneratecode.m
+++
@@ -1,4 +0,0 @@
-% generate MATLAB code for IQTools functions
-function iqgeneratecode(~, code)
-h = iqcodeview('text', sprintf('%%\n%% automatically generated code by IQTools\n%%\n%s\n', code));
-
--- iqtools_2023_10_24/iqtools/iqversion.m
+++
@@ -1,5 +0,0 @@
-function result = iqversion()
-% Returns IQtools version. Format YYYY.MM.DD
-% The string is updated by the mkapp script
-result = '2023.10.24';
-    
