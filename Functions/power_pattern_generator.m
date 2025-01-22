bb0809fa4df2d4711c78822a17bf8135e08f9a39
Seguar <1035707@gmail.com>
2025-01-22 16:56:50 +0200

    Polar plot added

--- Functions/power_pattern_generator.m
+++ Functions/power_pattern_generator.m
@@ -12,14 +12,23 @@ function pow_claibration_intrp = power_pattern_generator(meas_mat, scan_axis, in
     pow_claibration_intrp(:,3) = spline(scan_axis,pow_claibration_src(3,:), iterpulation_axis);
     pow_claibration_intrp(:,4) = spline(scan_axis,pow_claibration_src(4,:), iterpulation_axis);
     % pow_claibration_intrp = pow_claibration_intrp/(pow_claibration_src(1,7));
-%     figure(8);
-%     hold on;
-%     plot(iterpulation_axis,db(pow_claibration_intrp)/2,LineStyle="-", LineWidth=3);
-%     % plot(iterpulation_axis,pow_new,LineStyle="--", LineWidth=3);
-%     title('Power Pattern');
-%     xlabel('DOA (deg)');
-%     ylabel('Power (dBm)');
-%     legend('Ant 1','Ant 2','Ant 3','Ant 4', Location='nw');
-%     set(gca, fontsize=15)
-%     grid on;
+    figure
+    % hold on;
+    % plot(iterpulation_axis,db(pow_claibration_intrp)/2,LineStyle="-", LineWidth=3);
+    plot(iterpulation_axis,db(pow_claibration_intrp)/2);
+    % plot(iterpulation_axis,pow_new,LineStyle="--", LineWidth=3);
+    title('Power Pattern');
+    xlabel('DOA (deg)');
+    ylabel('Power (dB)');
+    legend('Ant 1','Ant 2','Ant 3','Ant 4', Location='best');
+    % set(gca, fontsize=15)
+    grid on;
+    figure
+    polarplot(deg2rad(iterpulation_axis), db(pow_claibration_intrp)/2);
+    ax = gca;
+    ax.ThetaZeroLocation = 'top'; % Set 0 degree angle at the top
+    ax.ThetaDir = 'clockwise';    % Rotate angles clockwise
+    ax.ThetaLim = [iterpulation_axis(1), iterpulation_axis(end)];
+    legend('Ant 1','Ant 2','Ant 3','Ant 4', Location='best');
+    % ax.ThetaTick = scan_axis;
 end
